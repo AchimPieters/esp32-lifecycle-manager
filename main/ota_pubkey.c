7cd2dfc532afd1fb005b130b1627d629dd71eb21
Achim Pieters <Achim.Pieters@outlook.com>
2025-09-09 18:51:28 +0200

    chore: update OTA public key

--- main/ota_pubkey.c
+++ main/ota_pubkey.c
@@ -2,7 +2,7 @@
 
 const unsigned char ota_pubkey_pem[] =
 "-----BEGIN PUBLIC KEY-----\n"
-"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEFNjyMmUCIFv3/WNEsihmvLIvqoho\n"
-"lhTyZ9o/cUGPyF6ftkX8kWQ8mERYw6y5FLNvba+QhHD1K33QOijM086HEg==\n"
+"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEdvNFFIe+YXpUxiwFYlWAy3M3t6Sa\n"
+"BP6750XmINFU950HVj8YfJIa/ILfYQKMxiCrhiyzcz09kkRKY8iW8zrfhQ==\n"
 "-----END PUBLIC KEY-----\n";
 const size_t ota_pubkey_pem_len = sizeof(ota_pubkey_pem);
