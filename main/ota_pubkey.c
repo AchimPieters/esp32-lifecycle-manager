2502d9f5ed873ecd9f024a24cb0f98c8a67595b4
Achim Pieters <Achim.Pieters@outlook.com>
2025-09-12 19:33:12 +0200

    Use SHA-384 hash signature and length for updates

--- main/ota_pubkey.c
+++
@@ -1,9 +0,0 @@
-#include "ota_pubkey.h"
-
-const unsigned char ota_pubkey_pem[] =
-"-----BEGIN PUBLIC KEY-----\n"
-"MHYwEAYHKoZIzj0CAQYFK4EEACIDYgAEhPap2UPkZ6n97Wr3uKHv6iU9OumtrQmG\n"
-"nBHWYATFD5a8QOilZSySo5QI/Ers7t5KmiwFTRB4HLLpLRKGiTqrgKNf/n+2HGCv\n"
-"60KaXmXtERmxnGrNF0i3cQDv6LczUdLK\n"
-"-----END PUBLIC KEY-----\n";
-const size_t ota_pubkey_pem_len = sizeof(ota_pubkey_pem);
