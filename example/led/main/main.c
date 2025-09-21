cdafc24a8498b6944e5170af821657b4a319ba18
Achim Pieters <info@studiopieters.nl>
2025-09-21 18:51:37 +0200

    Update
    Update

--- example/led/main/main.c
+++ example/led/main/main.c
@@ -21,135 +21,119 @@
    for more information visit https://www.studiopieters.nl
  **/
 
-#include <stdbool.h>
 #include <stdio.h>
-
-#include <esp_err.h>
 #include <esp_log.h>
-#include <driver/gpio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <driver/gpio.h>
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 
-#include "esp32-lcm.h"
+#include "esp32-lcm.h" // Add this line
 
+// LED control
 #define LED_GPIO CONFIG_ESP_LED_GPIO
+bool led_on = false;
 
+void led_write(bool on) {
+        gpio_set_level(LED_GPIO, on ? 1 : 0);
+}
+
+// All GPIO Settings
+void gpio_init() {
+        gpio_reset_pin(LED_GPIO); // Reset GPIO pin to avoid conflicts
+        gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+        led_write(led_on);
+}
+
+// Accessory identification
+void accessory_identify_task(void *args) {
+        for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 2; j++) {
+                        led_write(true);
+                        vTaskDelay(pdMS_TO_TICKS(100));
+                        led_write(false);
+                        vTaskDelay(pdMS_TO_TICKS(100));
+                }
+                vTaskDelay(pdMS_TO_TICKS(250));
+        }
+        led_write(led_on);
+        vTaskDelete(NULL);
+}
+
+void accessory_identify(homekit_value_t _value) {
+        ESP_LOGI("INFORMATION", "Accessory identify");
+        xTaskCreate(accessory_identify_task, "Accessory identify", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+}
+
+homekit_value_t led_on_get() {
+        return HOMEKIT_BOOL(led_on);
+}
+
+void led_on_set(homekit_value_t value) {
+        if (value.format != homekit_format_bool) {
+                ESP_LOGE("ERROR", "Invalid value format: %d", value.format);
+                return;
+        }
+        led_on = value.bool_value;
+        led_write(led_on);
+}
+
+// HomeKit characteristics
 #define DEVICE_NAME "HomeKit LED"
 #define DEVICE_MANUFACTURER "StudioPieters®"
 #define DEVICE_SERIAL "NLDA4SQN1466"
 #define DEVICE_MODEL "SD466NL/A"
-
-static const char *HOMEKIT_TAG = "HOMEKIT";
-
-static bool led_on = false;
-
-static void led_write(bool on) {
-    gpio_set_level(LED_GPIO, on ? 1 : 0);
-}
-
-static void accessory_identify_task(void *args) {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 2; j++) {
-            led_write(true);
-            vTaskDelay(pdMS_TO_TICKS(100));
-            led_write(false);
-            vTaskDelay(pdMS_TO_TICKS(100));
-        }
-        vTaskDelay(pdMS_TO_TICKS(250));
-    }
-    led_write(led_on);
-    vTaskDelete(NULL);
-}
-
-static void accessory_identify(homekit_value_t _value) {
-    ESP_LOGI(HOMEKIT_TAG, "Accessory identify");
-    xTaskCreate(accessory_identify_task, "Accessory identify", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
-}
-
-static homekit_value_t led_on_get() {
-    return HOMEKIT_BOOL(led_on);
-}
-
-static void led_on_set(homekit_value_t value) {
-    if (value.format != homekit_format_bool) {
-        ESP_LOGW(HOMEKIT_TAG, "Invalid value format: %d", value.format);
-        return;
-    }
-    led_on = value.bool_value;
-    led_write(led_on);
-}
+#define FW_VERSION "0.0.1"
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
-homekit_characteristic_t manufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER, DEVICE_MANUFACTURER);
+homekit_characteristic_t manufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER,  DEVICE_MANUFACTURER);
 homekit_characteristic_t serial = HOMEKIT_CHARACTERISTIC_(SERIAL_NUMBER, DEVICE_SERIAL);
 homekit_characteristic_t model = HOMEKIT_CHARACTERISTIC_(MODEL, DEVICE_MODEL);
-homekit_characteristic_t revision = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION,
-                                                            LIFECYCLE_DEFAULT_FW_VERSION);
-homekit_characteristic_t ota_trigger = API_OTA_TRIGGER;
+homekit_characteristic_t revision = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, LIFECYCLE_DEFAULT_FW_VERSION); //Adjust this line
+homekit_characteristic_t ota_trigger = API_OTA_TRIGGER; // Add this line
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Woverride-init"
 homekit_accessory_t *accessories[] = {
-    HOMEKIT_ACCESSORY(
-        .id = 1,
-        .category = homekit_accessory_category_lighting,
-        .services = (homekit_service_t*[]) {
-            HOMEKIT_SERVICE(
-                ACCESSORY_INFORMATION,
-                .characteristics = (homekit_characteristic_t*[]) {
-                    &name,
-                    &manufacturer,
-                    &serial,
-                    &model,
-                    &revision,
-                    HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
-                    NULL
-                }
-            ),
-            HOMEKIT_SERVICE(
-                LIGHTBULB,
-                .primary = true,
-                .characteristics = (homekit_characteristic_t*[]) {
-                    HOMEKIT_CHARACTERISTIC(NAME, "HomeKit LED"),
-                    HOMEKIT_CHARACTERISTIC(ON, false, .getter = led_on_get, .setter = led_on_set),
-                    NULL
-                }
-            ),
-            HOMEKIT_SERVICE(
-                CUSTOM_SETUP,
-                .characteristics = (homekit_characteristic_t*[]) {
-                    HOMEKIT_CHARACTERISTIC(NAME, "Lifecycle"),
-                    &ota_trigger,
-                    NULL
-                }
-            ),
-            NULL
-        }
-    ),
-    NULL
+        HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_lighting, .services = (homekit_service_t*[]) {
+                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
+                        &name,
+                        &manufacturer,
+                        &serial,
+                        &model,
+                        &revision,
+                        HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
+                        &ota_trigger,  // Add this line
+                        NULL
+                }),
+                HOMEKIT_SERVICE(LIGHTBULB, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
+                        HOMEKIT_CHARACTERISTIC(NAME, "HomeKit LED"),
+                        HOMEKIT_CHARACTERISTIC(ON, false, .getter = led_on_get, .setter = led_on_set),
+                        NULL
+                }),
+                NULL
+        }),
+        NULL
 };
 #pragma GCC diagnostic pop
 
 homekit_server_config_t config = {
-    .accessories = accessories,
-    .password = CONFIG_ESP_SETUP_CODE,
-    .setupId = CONFIG_ESP_SETUP_ID,
+        .accessories = accessories,
+        .password = CONFIG_ESP_SETUP_CODE,
+        .setupId = CONFIG_ESP_SETUP_ID,
 };
 
-static void on_wifi_ready(void) {
-    ESP_LOGI(HOMEKIT_TAG, "Starting HomeKit server...");
-    homekit_server_init(&config);
+void on_wifi_ready() {
+        ESP_LOGI("INFORMATION", "Starting HomeKit server...");
+        homekit_server_init(&config);
 }
 
 void app_main(void) {
-    ESP_ERROR_CHECK(lifecycle_nvs_init());
-    (void)lifecycle_configure_homekit(&revision, &ota_trigger, HOMEKIT_TAG);
 
-    gpio_reset_pin(LED_GPIO);
-    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-    led_write(led_on);
+        ESP_ERROR_CHECK(lifecycle_nvs_init()); // Add this line
+        (void)lifecycle_configure_homekit(&revision, &ota_trigger); // Add this line
 
-    ESP_ERROR_CHECK(wifi_start(on_wifi_ready));
+        wifi_init();
+        gpio_init();
 }
